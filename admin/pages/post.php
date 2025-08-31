33cacab03688a12f2733570b2f7d44b9c57fed21
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-08-31 11:46:28 +0100

    Auto-save changes

--- admin/pages/post.php
+++ admin/pages/post.php
@@ -163,7 +163,13 @@ $posts = $stmt->fetchAll();
         <form method="get" action="index.php" class="search-form">
           <input type="hidden" name="page" value="posts">
           <input type="text" name="q" placeholder="Search by title..." value="<?=htmlspecialchars($q)?>">
-          
+          <td>
+  <?= htmlspecialchars($p['title']) ?><br>
+  <a href="post_edit.php?id=<?= $p['id'] ?>" 
+     class="edit-link" 
+     data-id="<?= $p['id'] ?>">Edit Post</a>
+</td>
+
         </form>
         <button id="newPostBtn" class="btn-approve">
           <i class="bx bx-plus"></i> Add Article
