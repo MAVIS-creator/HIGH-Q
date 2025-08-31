7b12dcaa8373afa767848f33983801d5dac9de72
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-08-31 14:37:38 +0100

    Auto-save changes

--- admin/includes/csrf.php
+++ admin/includes/csrf.php
@@ -1,27 +1,12 @@
 <?php
 // admin/includes/csrf.php
-
-use Symfony\Component\Security\Csrf\CsrfTokenManager;
-use Symfony\Component\Security\Csrf\CsrfToken;
-
-require_once __DIR__ . '/../../vendor/autoload.php';
-
-session_start();
-
-$csrfTokenManager = new CsrfTokenManager();
-
-/**
- * Generate a CSRF token for a given form ID
- */
-function generateToken(string $id = 'default_form'): string {
-    global $csrfTokenManager;
-    return $csrfTokenManager->getToken($id)->getValue();
+function generateToken() {
+    if (empty($_SESSION['csrf_token'])) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['csrf_token'];
 }
 
-/**
- * Verify a submitted CSRF token
- */
-function verifyToken(string $id, string $token): bool {
-    global $csrfTokenManager;
-    return $csrfTokenManager->isTokenValid(new CsrfToken($id, $token));
+function verifyToken($token) {
+    return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
 }
