cd954b9692b4efe710851e7d04d9be699b4b3c05
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-08-31 19:19:41 +0100

    Auto-save changes

--- admin/pages/users.php
+++ admin/pages/users.php
@@ -2,7 +2,7 @@
 // admin/pages/users.php
 
 require '../includes/auth.php';
-require '../includes/db.php';
+require './admin/includes/db.php';
 require './includes/functions.php';
 require './includes/csrf.php';
 
