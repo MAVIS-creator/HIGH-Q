5de061a32aa4ff2a2eecdd5c54903ff5de138e48
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-09-01 16:27:15 +0100

    Auto-save changes

--- admin/includes/auth.php
+++ admin/includes/auth.php
@@ -1,15 +1,2 @@
 <?php
-// admin/includes/auth.php
-session_start();
-
-if (!isset($_SESSION['user'])) {
-    header("Location: ../login.php");
-    exit;
-}
-
-function requireRole($roles = []) {
-    if (!in_array($_SESSION['user']['role_slug'], $roles)) {
-        header("Location: index.php");
-        exit;
-    }
-}
+https://preview--high-q-admin-suite.lovable.app/auth
