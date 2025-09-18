0e98c363a3f96fe2d4d819d25fc78dc00c5d8110
gamerdave-web <opy.omo0108@gmail.com>
2025-09-18 13:57:48 +0100

    Auto-save 18/09/2025 13:57:48.29

--- admin/pages/courses.php
+++ admin/pages/courses.php
@@ -107,6 +107,7 @@ $tutors = $pdo->query("
   <meta charset="UTF-8">
   <title>Courses Management - HIGH Q SOLID ACADEMY</title>
   <link rel="stylesheet" href="../assets/css/admin.css">
+  <link rel="stylesheet" href="courses.css">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
