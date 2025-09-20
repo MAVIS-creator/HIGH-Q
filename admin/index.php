9433bc5dba746baacbd9954b7ba5d97dca86eb69
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-09-20 22:00:30 +0100

    Auto-save 20/09/2025 22:00:29.35

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@
     <title>HIGH Q SOLID ACADEMY — Admin Access</title>
 
     <!-- Favicon -->
-        <link rel="shortcut icon" href="/HIGH-Q/admin/assets/img/hq-favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="/HIGH-Q/admin/assets/img/favicon.ico" type="image/x-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;800&display=swap" rel="stylesheet">
