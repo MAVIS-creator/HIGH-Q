5adb107d1a5b63a5ff06aad6b411ee5e3af502b2
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-09-20 21:39:41 +0100

    Auto-save 20/09/2025 21:39:41.51

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -30,7 +30,7 @@ $menuItems = [
 
 <aside class="admin-sidebar">
     <div class="sidebar-logo">
-        <img src="../public/assets/images/hq-logo.jpeg" alt="Academy Logo">
+    <img src="../assets/img/hq-logo.jpeg" alt="Academy Logo">
         <h3>HIGH Q SOLID ACADEMY</h3>
     <small><?= htmlspecialchars($_SESSION['user']['role_name'] ?? ''); ?></small>
     </div>
