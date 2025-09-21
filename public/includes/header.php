d9c818e692368f6dff781589cd0f01bcff106a1f
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-09-22 00:17:27 +0100

    Auto-save 22/09/2025  0:17:27.37

--- public/includes/header.php
+++ public/includes/header.php
@@ -27,7 +27,7 @@ if (file_exists(__DIR__ . '/../config/db.php')) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="assets/css/public.css">
-    <link rel="shortcut icon" href="/HIGH-Q/admin/assets/img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/img/favicon.ico" type="image/x-icon">
 </head>
 
 <body>
