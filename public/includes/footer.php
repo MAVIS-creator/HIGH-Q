4d861905baef38613dc8c3d2e70491137b342642
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-09-23 04:19:12 +0100

    Auto-save 23/09/2025  4:19:11.21

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -27,7 +27,7 @@
     <div class="footer-links">
       <h3>Quick Links</h3>
       <ul>
-        <li><a href="">Home</a></li>
+        <li><a href="index.php">Home</a></li>
         <li><a href="about.php">About Us</a></li>
         <li><a href="programs.php">Programs</a></li>
         <li><a href="register.php">Admission</a></li>
