a7a1c0a8752403df71d9c28c1a2899263ba8858a
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-09-28 03:07:44 +0100

    Auto-save 28/09/2025  3:07:43.71

--- tools/inspect_site_settings.php
+++ tools/inspect_site_settings.php
@@ -3,7 +3,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 $host = $_ENV['DB_HOST'];
-db = $_ENV['DB_NAME'];
+$db = $_ENV['DB_NAME'];
 $user = $_ENV['DB_USER'];
 $pass = $_ENV['DB_PASS'];
 $charset = $_ENV['DB_CHARSET'] ?? 'utf8mb4';
