e896932d05957659fb68026df681f904e3f1dfb4
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-09-29 21:30:08 +0100

    Auto-save 29/09/2025 21:30:07.89

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -26,6 +26,7 @@ $menuItems = [
     'post'      => ['title' => 'News / Blog', 'icon' => 'bx bxs-news', 'url' => 'index.php?pages=post'],
     'comments'  => ['title' => 'Comments', 'icon' => 'bx bxs-comment-detail', 'url' => 'index.php?pages=comments'],
     'chat'      => ['title' => 'Chat Support', 'icon' => 'bx bxs-message-dots', 'url' => 'index.php?pages=chat'],
+    'audit_logs' => ['title' => 'Audit Logs', 'icon' => 'bx bxs-report', 'url' => 'index.php?pages=audit_logs'],
 ];
 ?>
 
