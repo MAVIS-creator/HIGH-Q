b71a25fc85cc113c22387ee10bef92d10e1d0bdf
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-10-06 11:35:30 +0100

    Auto-save 06/10/2025 11:35:27.99

--- public/includes/header.php
+++ public/includes/header.php
@@ -220,13 +220,13 @@ if (file_exists(__DIR__ . '/../config/db.php')) {
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
   <!-- Custom CSS -->
-  <link rel="stylesheet" href="./assets/css/public.css">
-  <link rel="stylesheet" href="./assets/css/responsive.css">
-  <link rel="stylesheet" href="./assets/css/ceo-responsive.css">
-  <link rel="stylesheet" href="./assets/css/animations.css">
-  <link rel="stylesheet" href="./assets/css/social-icons.css">
-  <link rel="stylesheet" href="./assets/css/post-toc.css">
-  <link rel="stylesheet" href="./assets/css/hero.css">
+  <link rel="stylesheet" href="../">
+  <link rel="stylesheet" href="../assets/css/responsive.css">
+  <link rel="stylesheet" href="../assets/css/ceo-responsive.css">
+  <link rel="stylesheet" href="../assets/css/animations.css">
+  <link rel="stylesheet" href="../assets/css/social-icons.css">
+  <link rel="stylesheet" href="../assets/css/post-toc.css">
+  <link rel="stylesheet" href="../assets/css/hero.css">
   <link rel="shortcut icon" href="./assets/images/favicon.ico" type="image/x-icon">
   <!-- SweetAlert2 -->
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
