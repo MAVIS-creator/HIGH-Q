5c1857a8183aa642234cac052509cc92ca0d4939
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-10-06 11:32:48 +0100

    Auto-save 06/10/2025 11:32:48.54

---
+++ index.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="<?= $baseUrl ?>/assets/css/public.css">
+<link rel="stylesheet" href="<?= $baseUrl ?>/assets/css/responsive.css">
+<link rel="stylesheet" href="<?= $baseUrl ?>/assets/css/ceo-responsive.css">
+<link rel="stylesheet" href="<?= $baseUrl ?>/assets/css/animations.css">
+<link rel="stylesheet" href="<?= $baseUrl ?>/assets/css/social-icons.css">
+<link rel="stylesheet" href="<?= $baseUrl ?>/assets/css/post-toc.css">
+<link rel="stylesheet" href="<?= $baseUrl ?>/assets/css/hero.css">
+<link rel="shortcut icon" href="<?= $baseUrl ?>/assets/images/favicon.ico" type="image/x-icon">
