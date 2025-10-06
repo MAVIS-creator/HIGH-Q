786b36539770700d3a221e5f160c9777d3d308dc
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-10-06 01:50:13 +0100

    Auto-save 06/10/2025  1:50:11.84

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -45,6 +45,7 @@ if (!headers_sent()) {
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="../assets/css/notifications.css">
+    <link rel="stylesheet" href="../assets/css/admin-style.css">
     <!-- SweetAlert2 (used by many admin pages) -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
