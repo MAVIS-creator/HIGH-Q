54fed6a33167b6567654caf32314f6baa713ec44
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-10-07 11:05:50 +0100

    Auto-save 07/10/2025 11:05:49.51

--- public/includes/header.php
+++ public/includes/header.php
@@ -269,52 +269,7 @@ if (file_exists(__DIR__ . '/../config/db.php')) {
             <i class="bx bx-menu"></i>
           </button>
 
-          <!-- Navigation -->
-          <div class="collapse navbar-collapse" id="mainNav">
-            <?php $cur = basename($_SERVER['PHP_SELF'] ?? '') ?>
-            <ul class="navbar-nav mx-auto">
-              <li class="nav-item">
-                <a class="nav-link <?= $cur === 'index.php' ? 'active' : '' ?>" href="index.php">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link <?= $cur === 'about.php' ? 'active' : '' ?>" href="about.php">About Us</a>
-              </li>
-
-              <!-- Programs Dropdown -->
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
-                  Programs
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="programs.php">Programs</a></li>
-                  <li><a class="dropdown-item" href="exams.php">Exams</a></li>
-                </ul>
-              </li>
-
-              <!-- News Dropdown -->
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
-                  News
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="news.php">News & Blog</a></li>
-                  <li><a class="dropdown-item" href="community.php">Community</a></li>
-                </ul>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link <?= $cur === 'register.php' ? 'active' : '' ?>" href="register.php">Admission</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link <?= $cur === 'contact.php' ? 'active' : '' ?>" href="contact.php">Contact</a>
-              </li>
-            </ul>
-
-            <!-- Register Button -->
-            <div class="d-none d-lg-block">
-              <a href="register.php" class="btn btn-primary">Register Now</a>
-            </div>
-          </div>
+          <?php $cur = basename($_SERVER['PHP_SELF'] ?? '') ?>
         </nav>
       </div>
     </div>
