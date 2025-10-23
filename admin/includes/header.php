2bad3684da1aa64de90f9e813a4f37f17e5d68f1
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-10-23 18:52:36 +0100

    Auto-save 23/10/2025 18:52:36.39

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -44,7 +44,7 @@ if (!headers_sent()) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    lin
+    <link rel="stylesheet" href=".css">
     <link rel="stylesheet" href="../assets/css/admin-style.css">
     <link rel="stylesheet" href="../assets/css/notifications.css">
     <link rel="stylesheet" href="../assets/css/responsive.css">
