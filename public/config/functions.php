5b746146f6a4db601e4d0ecccc1bca848376ed49
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-10-25 13:18:28 +0100

    Auto-save 25/10/2025 13:18:28.46

--- public/config/functions.php
+++ public/config/functions.php
@@ -10,6 +10,28 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
 
+// Compute a canonical base URL for use when building absolute links.
+// Preference order: APP_URL from env -> computed from request (scheme + host + optional script dir)
+if (!isset($GLOBALS['HQ_BASE_URL'])) {
+    $hq = null;
+    if (!empty($_ENV['APP_URL'])) {
+        $hq = rtrim($_ENV['APP_URL'], '/');
+    } else {
+        $proto = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+        $host = $_SERVER['HTTP_HOST'] ?? 'localhost';
+        // If the application runs from a subdirectory, APP_URL should be used; fallback to host-only base
+        $hq = rtrim($proto . '://' . $host, '/');
+    }
+    $GLOBALS['HQ_BASE_URL'] = $hq;
+}
+
+/**
+ * Helper to get the computed base URL
+ */
+function hq_base_url(): string {
+    return $GLOBALS['HQ_BASE_URL'] ?? '';
+}
+
 /**
  * Log actions into audit_logs
  */
