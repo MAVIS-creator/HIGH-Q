4de6c8fbc92bc988d1174450001254fe690880b1
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-11-02 04:17:49 +0100

    Auto-save 02/11/2025  4:17:49.13

--- admin/pages/payment.php
+++ admin/pages/payment.php
@@ -1,6 +1,6 @@
 <?php
 // admin/pages/payment.php - small admin UI to create & send payment links
-require_once __DIR__ . '/../includes/auth.php';
+require_once __DIR__ . '/../bootstrap.php';
 require_once __DIR__ . '/../includes/csrf.php';
 require_once __DIR__ . '/../bootstrap.php';
 require_once __DIR__ . '/../includes/functions.php';
