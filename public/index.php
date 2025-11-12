eec8829e379485ddccb1460a803866af344bad26
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-11-12 21:05:34 +0100

    Auto-save 12/11/2025 21:05:34.41

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
+// Normalize accidental duplicate /public/public in the URL early
+if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '/public/public/') !== false) {
+	$normalized = preg_replace('#/public/public/#', '/public/', $_SERVER['REQUEST_URI'], 1);
+	header('Location: ' . $normalized, true, 301);
+	exit;
+}
 // public/index.php - canonical homepage loader
 // We'll include the home content here. This file is the webserver's default index.
 $pageTitle = 'High Q Solid Academy - Home';
