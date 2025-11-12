328e7206873995141ecdaeb0d7e6db6e3d7a85f0
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-11-12 23:38:29 +0100

    Auto-save 12/11/2025 23:38:28.93

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -22,6 +22,7 @@ $menuItems = [
     'tutors'    => ['title' => 'Tutors', 'icon' => 'bx bxs-chalkboard', 'url' => 'index.php?pages=tutors'],
     'students'  => ['title' => 'Students', 'icon' => 'bx bxs-graduation', 'url' => 'index.php?pages=students'],
     'payments'  => ['title' => 'Payments', 'icon' => 'bx bxs-credit-card', 'url' => 'index.php?pages=payments'],
+    'appointments' => ['title' => 'Appointments', 'icon' => 'bx bx-calendar', 'url' => 'appointments.php'],
     'create_payment_link' => ['title' => 'Create Payment Link', 'icon' => 'bx bx-link', 'url' => 'index.php?pages=payment'],
     'icons'     => ['title' => 'Icons', 'icon' => 'bx bx-image', 'url' => 'index.php?pages=icons'],
     'post'      => ['title' => 'News / Blog', 'icon' => 'bx bxs-news', 'url' => 'index.php?pages=post'],
