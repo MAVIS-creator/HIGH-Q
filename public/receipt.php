d8a6c0f9f60f2b0c8d089c4a365da42780b95e15
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-12-27 03:26:21 +0100

    Auto-save 27/12/2025  3:26:21.43

--- public/receipt.php
+++ public/receipt.php
@@ -2,6 +2,7 @@
 // Unified receipt (HTML + optional PDF)
 require_once __DIR__ . '/config/db.php';
 require_once __DIR__ . '/config/functions.php';
+require_once __DIR__ . '/includes/welcome-kit-generator.php';
 
 use Dompdf\Dompdf;
 use Dompdf\Options;
