aeafaf1713226825d8edde37919900f97ef21ab1
David Omotosho <opy.omo0108@gmail.com>
2026-01-01 15:52:48 +0100

    Auto-save Thu 01/01/2026 15:52:48.36

--- migrations/2025-10-23-add-waec_serial_column_mysql.sql
+++ migrations/2025-10-23-add-waec_serial_column_mysql.sql
@@ -1,11 +1,3 @@
--- Migration: add waec_serial column and copy data from waec_serial_no if present
--- Idempotent and safe to run multiple times
+-- Migration: add waec_serial column
 
-ALTER TABLE `post_utme_registrations`
-  ADD COLUMN IF NOT EXISTS `waec_serial` VARCHAR(100) DEFAULT NULL;
-
--- Copy values only if source column exists and target is empty
-UPDATE post_utme_registrations SET waec_serial = waec_serial_no 
-WHERE waec_serial IS NULL 
-  AND waec_serial_no IS NOT NULL 
-  AND waec_serial_no <> '';
+ALTER TABLE post_utme_registrations ADD COLUMN IF NOT EXISTS waec_serial VARCHAR(100);
