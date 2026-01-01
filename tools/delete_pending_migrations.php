97849493929106444d0ff66e0a75a63c41dd1394
David Omotosho <opy.omo0108@gmail.com>
2026-01-01 15:54:09 +0100

    Auto-save Thu 01/01/2026 15:54:09.43

---
+++ tools/delete_pending_migrations.php
@@ -0,0 +1,19 @@
+<?php
+require 'public/config/db.php';
+
+// Delete the pending migrations so they can be re-applied fresh
+$migrations = [
+  '2025-10-04-make-payments-id-autoinc.sql',
+  '2025-10-05-add-contact-tiktok-column.sql',
+  '2025-10-23-add-waec_serial_column_mysql.sql',
+  '2025-10-26-create-postutme-and-payments-columns_mysql.sql',
+  '2025-12-15-add-topic-to-forum-questions.sql',
+  '2025-12-23-upsert-program-slugs.sql',
+  'seed_icons.php'
+];
+
+foreach ($migrations as $m) {
+  $pdo->execute($pdo->prepare("DELETE FROM migrations WHERE filename = ?"), [$m]);
+}
+
+echo "Deleted " . count($migrations) . " pending migrations. Ready to re-run.\n";
