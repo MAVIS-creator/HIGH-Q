5f6d65f03e0899339475a841675b77a8fbfee641
David Omotosho <opy.omo0108@gmail.com>
2026-01-01 16:04:17 +0100

    Auto-save Thu 01/01/2026 16:04:17.32

--- migrations/2025-10-04-make-payments-id-autoinc.sql
+++ migrations/2025-10-04-make-payments-id-autoinc.sql
@@ -2,22 +2,6 @@
 -- Safe to run multiple times
 
 ALTER TABLE payments MODIFY COLUMN id INT NOT NULL AUTO_INCREMENT;
-  EXECUTE stmt;
-  DEALLOCATE PREPARE stmt;
-
-END$$
-
-DELIMITER ;
-
--- Execute the procedure (will abort with a clear error message if pre-checks fail)
-CALL migrate_payments_autoinc();
-
--- Cleanup
-DROP PROCEDURE IF EXISTS migrate_payments_autoinc();
-
--- OPTIONAL: If you need to assign ids to rows with id=NULL or id=0, you can run the following (only after backup):
--- SET @max = (SELECT IFNULL(MAX(id), 0) FROM payments);
--- UPDATE payments
 -- SET id = (@max := @max + 1)
 -- WHERE id IS NULL OR id = 0
 -- ORDER BY created_at ASC;
