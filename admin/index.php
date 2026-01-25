0274b8c9a4c9755bb7512a925c3707989b14f570
MAVIS-creator <akintunde.dolapo1@gmail.com>
2026-01-25 18:12:06 +0100

    Auto-save Sun 01/25/2026 18:12:06.20

--- admin/index.php
+++ admin/index.php
@@ -16,6 +16,8 @@ if (isset($_GET['pages'])) {
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/auth.css">
+    <link rel="stylesheet" href="./assets/css/admin-minimal.css">
+    <script src="./assets/js/device-capability.js"></script>
     <style>
         /* Landing page specific styles */
         .landing-container {
