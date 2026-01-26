ab8d8fc3ff4a8bfbe2dd9c81fcec51fe8f319c6a
MAVIS-creator <akintunde.dolapo1@gmail.com>
2026-01-26 01:06:59 +0100

    Auto-save Mon 01/26/2026  1:06:59.27

--- debug_settings.php
+++ debug_settings.php
@@ -1,6 +1,6 @@
 <?php
 // Quick debug script
-require_once __DIR__ . '/config/db.php';
+require_once __DIR__ . '/public/config/db.php';
 
 echo "Site Settings columns:\n";
 $stmt = $pdo->query("DESCRIBE site_settings");
