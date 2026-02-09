ce66235e813e8dea6f1e001db4b6ee2db8febac2
MAVIS-creator <akintunde.dolapo1@gmail.com>
2026-02-09 09:22:10 +0000

    Auto-save 09/02/2026  9:22:09.87

--- public/about.php
+++ public/about.php
@@ -33,9 +33,6 @@ $pageKeywords = 'about High Q Tutorial, exam coaching Nigeria, JAMB tutors, educ
   </div>
 </section>
 
-<?php include __DIR__ . '/tutors.php'; ?>
-<?php include __DIR__ . '/includes/footer.php'; ?>
-
 <!-- Core Values -->
 
 <section class="history-section">
